8851262e790fd59f3743a62778a9b671fbffcb80
allyusd <allyusd@yahoo.com.tw>
2018-08-26 21:37:49 +0800

    test: Fifteen_All

--- TennisGameSimple/TennisGameTest.cs
+++ TennisGameSimple/TennisGameTest.cs
@@ -49,6 +49,14 @@ namespace TennisGameSimple
             ScoreShouldBe("Love Thirty");
         }
 
+        [TestMethod]
+        public void Fifteen_All()
+        {
+            GivenFirstPlayerScoreTimes(1);
+            GivenSecondPlayerScoreTimes(1);
+            ScoreShouldBe("Fifteen All");
+        }
+
         private void GivenSecondPlayerScoreTimes(int times)
         {
             for (var i = 0; i < times; i++)
