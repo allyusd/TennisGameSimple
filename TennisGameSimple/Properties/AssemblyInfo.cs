b4c25a278f16a17cd80e6ee6f0e890c8b912fdc6
allyusd <allyusd@yahoo.com.tw>
2018-08-26 20:51:26 +0800

    init: init project

---
+++ TennisGameSimple/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TennisGameSimple")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Toshiba")]
+[assembly: AssemblyProduct("TennisGameSimple")]
+[assembly: AssemblyCopyright("Copyright © Toshiba 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bb507604-c686-456d-8700-51362cee30fb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
