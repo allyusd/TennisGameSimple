40c092e56ab5ba7b88cc99684709050ece7366a8
allyusd <allyusd@yahoo.com.tw>
2018-08-26 21:16:14 +0800

    feat: Fifteen_Love

--- TennisGameSimple/TennisGame.cs
+++ TennisGameSimple/TennisGame.cs
@@ -12,6 +12,11 @@ namespace TennisGameSimple
 
         public string Score()
         {
+            if (_firstPlayerScore == 1)
+            {
+                return "Fifteen Love";
+            }
+
             return "Love All";
         }
 
