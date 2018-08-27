72a7425d070627dac4633d0a097262e97700f07a
allyusd <allyusd@yahoo.com.tw>
2018-08-28 06:36:15 +0800

    test: Love_All

---
+++ TennisGameSimple/TennisGameTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisGameSimple
+{
+    [TestClass]
+    public class TennisGameTest
+    {
+        [TestMethod]
+        public void Love_All()
+        {
+            TennisGame tennisGame = new TennisGame();
+            var score = tennisGame.Score();
+            Assert.AreEqual("Love All", score);
+        }
+    }
+}
