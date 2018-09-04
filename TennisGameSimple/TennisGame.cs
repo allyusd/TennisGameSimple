7f3c6410825ac4e719bdb50c848f0ff1385ffb4b
allyusd <allyusd@yahoo.com.tw>
2018-09-04 23:51:35 +0800

    feat: Thirty_All

--- TennisGameSimple/TennisGame.cs
+++ TennisGameSimple/TennisGame.cs
@@ -27,6 +27,11 @@ namespace TennisGameSimple
                 return "Fifteen All";
             }
 
+            if (_firstPlayerScoreTimes == 2)
+            {
+                return "Thirty All";
+            }
+
             return "Love All";
         }
 
