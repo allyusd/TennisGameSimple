cefc418f77c0df5a9159fbe20253d817cbd2090d
allyusd <allyusd@yahoo.com.tw>
2018-09-04 23:53:05 +0800

    refactor

--- TennisGameSimple/TennisGame.cs
+++ TennisGameSimple/TennisGame.cs
@@ -22,17 +22,7 @@ namespace TennisGameSimple
                 return _scoreLookup[_firstPlayerScoreTimes] + " " + _scoreLookup[_secondPlayerScoreTimes];
             }
 
-            if (_firstPlayerScoreTimes == 1)
-            {
-                return "Fifteen All";
-            }
-
-            if (_firstPlayerScoreTimes == 2)
-            {
-                return "Thirty All";
-            }
-
-            return "Love All";
+            return _scoreLookup[_firstPlayerScoreTimes] + " All";
         }
 
         public void FirstPlayerScore()
