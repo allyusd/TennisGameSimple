b5a242b1cd49c949844ae632d152e3e2c0986aca
allyusd <allyusd@yahoo.com.tw>
2018-09-04 23:30:46 +0800

    test: Fifteen_Love

--- TennisGameSimple/TennisGame.cs
+++ TennisGameSimple/TennisGame.cs
@@ -2,9 +2,16 @@
 {
     public class TennisGame
     {
+        private int m_firstPlayerScoreTimes;
+
         public string Score()
         {
             return "Love All";
+        }
+
+        public void FirstPlayerScore()
+        {
+            m_firstPlayerScoreTimes++;
         }
     }
 }
--- TennisGameSimple/TennisGameTest.cs
+++ TennisGameSimple/TennisGameTest.cs
@@ -14,6 +14,13 @@ namespace TennisGameSimple
             ScoreShouldBe("Love All");
         }
 
+        [TestMethod]
+        public void Fifteen_Love()
+        {
+            game.FirstPlayerScore();
+            ScoreShouldBe("Fifteen Love");
+        }
+
         private void ScoreShouldBe(string expected)
         {
             var score = game.Score();
