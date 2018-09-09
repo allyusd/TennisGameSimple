7b0532b0917d0e85a81e5e0a382ea602a196c453
allyusd <allyusd@yahoo.com.tw>
2018-09-09 13:57:06 +0800

    test: Fifteen_Love

--- TennisGameSimple/TennisGame.cs
+++ TennisGameSimple/TennisGame.cs
@@ -2,9 +2,16 @@
 {
     public class TennisGame
     {
+        private int _firstPlayerScoreTimes;
+
         public string Score()
         {
             return "Love All";
+        }
+
+        public void FirstPlayerScore()
+        {
+            _firstPlayerScoreTimes++;
         }
     }
 }
